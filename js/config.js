6e556331f687c7d4af5cbe78222df2d2a8f9dfcc
vox403 <java960419@gmail.com>
2026-06-23 00:13:55 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const SUPABASE_URL = "https://gvuhbxpkmyiakyfhuish.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_uCymLmzy-3NR9JyOL-9DtA_HmkGb0-k";
-export const PORTAL_STORAGE_KEY = "voxtek_employee_v1";
-
-export const MAX_NAME_LENGTH = 24;
-export const RANK_LIMIT = 40;
